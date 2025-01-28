61072b72f4f497506f9db502b97db323fea9ec79
Roman Matveev <romiusse@yandex-team.ru>
2025-01-28 16:00:23 +0300

    sample

--- app/src/main/java/com/example/glancenovalauncherbug/GlanceWidget.kt
+++ app/src/main/java/com/example/glancenovalauncherbug/GlanceWidget.kt
@@ -4,12 +4,14 @@ import android.content.Context
 import androidx.compose.ui.graphics.Color
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
+import androidx.glance.LocalSize
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.SizeMode
 import androidx.glance.appwidget.provideContent
 import androidx.glance.background
 import androidx.glance.layout.Box
 import androidx.glance.layout.fillMaxSize
+import androidx.glance.layout.size
 import androidx.glance.unit.ColorProvider
 
 class GlanceWidget : GlanceAppWidget() {
@@ -19,8 +21,9 @@ class GlanceWidget : GlanceAppWidget() {
 
     override suspend fun provideGlance(context: Context, id: GlanceId) {
         provideContent {
+            val size = LocalSize.current
             Box(modifier = GlanceModifier
-                .fillMaxSize()
+                .size(size.width, size.height)
                 .background(ColorProvider(Color.Red))
             ) { /* Nothing */ }
         }
